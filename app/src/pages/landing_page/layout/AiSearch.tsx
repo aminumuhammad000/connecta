38a728bab6c62b07289fab5d5416fe8cc285c28c
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-10-16 14:11:30 +0100

    updated the landing page

--- app/src/pages/landing_page/layout/AiSearch.tsx
+++ app/src/pages/landing_page/layout/AiSearch.tsx
@@ -2,16 +2,16 @@ import { Icon } from "@iconify/react"
 import styles from "../../../styles/layouts/AiSearch.module.css";
 import aisearch from "../../../assets/aisearch.png";
 
+
 const AiSearch = () => {
   return (
     <div className={styles.AiSearch}>
-        {/* <br /> */}
         <img src={aisearch} alt="ai image" className={styles.image}/>
         <p className={styles.description}>Connecta uses AI to match freelancers with the right projects helping freelancers grow and clients hire quickly.</p>
         
         <div className={styles.searchContainer}>
             <textarea name="search" placeholder="To begin, please provide a description of the task you require." className={styles.textarea}></textarea>
-            <button className={styles.search}>Search <span><Icon  icon="mingcute:search-ai-line" className={styles.icon}/></span></button>
+            <button className={styles.search}>Search <span><Icon  icon="mingcute:search-ai-line" className={styles.icon} /></span></button>
         </div>
     </div>
   )
