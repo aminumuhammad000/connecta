666c4ede78224fc022892690d5fe3c76b62c339d
aminumuhammad000 <aminumuhammad00015@gmail.com>
2026-04-24 11:12:30 +0100

    removed some files

--- connecta-scraper/src/scrapers/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all scrapers here
-export { BaseScraper } from "./base.scraper";
-export { JobbermanScraper } from "./jobberman.scraper";
-export { ExampleScraper } from "./example.scraper";
-export { TestScraper } from "./test.scraper";
-export { MyJobMagScraper } from "./myjobmag.scraper";
-export { WeWorkRemotelyScraper } from "./weworkremotely.scraper";
